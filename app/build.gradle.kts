bd29a27ec5595738bf87d34817ecf3b17f0f096b
Divyesh <divyeshparkhiya33934@gmail.com>
2025-07-01 17:18:09 +0530

    CameraX Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,14 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+
     implementation(libs.zxing.android.embedded)
     implementation(libs.core)
+
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
 }
